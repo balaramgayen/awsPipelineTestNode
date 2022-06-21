8e3f4497042c2b4ca599dcf14aa8f81d4193d367
balaramgayen <balaramgayen53501@gmail.com>
2022-06-21 16:04:53 +0530

    first commit with initial setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Welcome</h1></br><h6>go to route /hello</h6>");
+});
+
+app.get("/hello", (req, res) => {
+  res.status(200).send("<h1>Hello AWS code pipeline</h1>");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`backend running on ${PORT}`);
+});
