b4fa6fad8c93590527abd4a05d6c39c3cf9592ae
balaramgayen <balaramgayen53501@gmail.com>
2022-06-21 16:30:18 +0530

    second commit -- changes in text under /hello route

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.status(200).send("<h1>Hello AWS code pipeline</h1>");
+  res.status(200).send("<h1>This text is after second changes on code</h1>");
 });
 
 const PORT = process.env.PORT || 3000;
